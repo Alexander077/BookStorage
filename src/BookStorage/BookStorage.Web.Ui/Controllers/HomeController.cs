b990a5e08de57a71b3c2ff06f218e2c984eacc49
Alexander077 <renzy.max@gamil.com>
2018-03-19 15:33:56 +0200

    small HomeController cleanup

--- src/BookStorage/BookStorage.Web.Ui/Controllers/HomeController.cs
+++ src/BookStorage/BookStorage.Web.Ui/Controllers/HomeController.cs
@@ -19,10 +19,6 @@ namespace BookStorage.Web.Ui.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
-            //BookDTO asd = _bookService.FindBook(1);
-            //List<BookDTO> asd = _bookService.GetAllBooks().ToList();
-
             return View();
         }
     }
