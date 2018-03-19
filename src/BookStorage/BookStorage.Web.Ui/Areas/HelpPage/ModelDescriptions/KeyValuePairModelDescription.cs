6ce8fd0d3a6b5782b8e5518f30500d67ff63d55b
Alexander077 <renzy.max@gamil.com>
2018-03-19 15:47:43 +0200

    BookStorage.Web.Ui project cleanup done

--- src/BookStorage/BookStorage.Web.Ui/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace BookStorage.Web.Ui.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- src/BookStorage/BookStorage.Web.Ui/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BookStorage.Web.Ui.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
